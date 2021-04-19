63d656bf200d99ac111b56040fb60c58d3f22b17
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-04-19 22:58:54 +0530

    Update build_and_deployment_pipeline.groovy

--- vars/build_and_deployment_pipeline.groovy
+++ vars/build_and_deployment_pipeline.groovy
@@ -15,6 +15,64 @@ def call(Map pipelineParams) {
                 ''' 
             echo "Checkout is completed!"
           }
+          
+          stage("Build") {
+             if(pipelineParams.APP_TYPE == "JAVA")
+              {
+                  sh '''
+                      cd $REPO
+                      mvn deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY} -Dmaven.test.skip=true
+                  '''
+              }
+             else {
+                      echo "No build for the APP_TYPE"+pipelineParams.APP_TYPE
+             }
+          }
+           stage("Static Code Analysis") {
+           }
+           stage("Delete Previous Deployment") {
+             
+              withCredentials([usernamePassword(credentialsId: 'dev-k8s-master', passwordVariable: 'pwd', usernameVariable: 'user')]) { 
+                sh '''
+                    echo Curiosity4ERP# | sudo -S kubectl delete deployment $ENVIRONMENT-$APP_NAME-deployment -n $NAMESPACE
+                '''
+    }
+
+              stage("Deploy") {
+                
+                def fileWrite = libraryResource "app-service.yaml"
+	writeFile file: "${WORKSPACE}/${REPO}/app-service.yaml", text: fileWrite
+    withCredentials([usernamePassword(credentialsId: 'dev-k8s-master', passwordVariable: 'pwd', usernameVariable: 'user')]) { 
+        sh '''
+            cd ${WORKSPACE}/${REPO}/
+            sed -i "s;%APP_NAME%;${APP_NAME};" app-service.yaml
+            sed -i "s;%NAMESPACE%;${NAMESPACE};" app-service.yaml
+            sed -i "s;%DOCKER_REGISTRY%;${DOCKER_REGISTRY};" app-service.yaml
+            sed -i "s;%CONTAINER_PORT%;${CONTAINER_PORT};" app-service.yaml
+            sed -i "s;%HOST_PORT%;${HOST_PORT};" app-service.yaml
+            sed -i "s;%HOST_IP%;${HOST_IP};" app-service.yaml
+            sed -i "s;%REPO%;${REPO};" app-service.yaml
+            sed -i "s;%REPLICAS%;${REPLICAS};" app-service.yaml
+            sed -i "s;%ENVIRONMENT%;${ENVIRONMENT};" app-service.yaml
+            echo Curiosity4ERP#| sudo -S kubectl apply -f app-service.yaml
+        '''
+    }
+              }
+             if(pipelineParams.EMAIL_TO_LIST?.trim()){   
+          echo "email send enabled"   
+          //new sendEmail().call(pipelineParams,"SUCCESS")   
+        } 
+}
         }
       }
+       }catch (err) {
+    echo "in catch block" 
+    echo "Caught: ${err}" 
+    currentBuild.result = 'FAILURE' 
+    if(pipelineParams.EMAIL_TO_LIST?.trim()){   
+      echo "email send enabled"   
+      //sendEmail().call(pipelineParams,"FAILURE")   
+    }  
+    throw err
+  }
     }
